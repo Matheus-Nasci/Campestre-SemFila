01b5219edd220eb5b6ffd864438633c04f60210a
José Paulo <jose.silva@bandtec.com.br>
2023-02-13 13:15:23 -0300

    first commit

---
+++ campestre-api/src/main/java/br/com/campestre/campestreapi/CampestreApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.campestre.campestreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampestreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampestreApiApplication.class, args);
+	}
+
+}
