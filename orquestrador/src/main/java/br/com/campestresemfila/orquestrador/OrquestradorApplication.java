6dabe11c124aa960cc0d8fc09f31b0944f41784e
José Paulo <jose.silva@bandtec.com.br>
2023-03-19 22:19:25 -0300

    orquestrador back-end

---
+++ orquestrador/src/main/java/br/com/campestresemfila/orquestrador/OrquestradorApplication.java
@@ -0,0 +1,13 @@
+package br.com.campestresemfila.orquestrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrquestradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrquestradorApplication.class, args);
+	}
+
+}
