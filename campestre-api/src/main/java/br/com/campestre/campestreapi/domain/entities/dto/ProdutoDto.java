c3b93ba3e99e8ee3e3e5c1b05393b5e52b1b5c89
José Paulo <jose.silva@bandtec.com.br>
2023-03-19 22:21:16 -0300

    classe de configuração do swagger

---
+++ campestre-api/src/main/java/br/com/campestre/campestreapi/domain/entities/dto/ProdutoDto.java
@@ -0,0 +1,10 @@
+package br.com.campestre.campestreapi.domain.entities.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProdutoDto {
+    private int idProduto;
+}
