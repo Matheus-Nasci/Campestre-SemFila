08b528ca6f473629898870f458be03afad914c33
José Paulo <jose.silva@bandtec.com.br>
2023-05-13 23:37:28 -0300

    pagina nova

---
+++ campestre-sem-fila/src/pages/AdicionarProduto/AdicionarProdutoPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import AdicionarProduto from "../../components/adicionar-produto/AdicionarProduto";
+
+class AdicionarProdutoPage extends Component {
+    render() {
+        return (
+            <>
+            <AdicionarProduto />
+            </>
+        )
+    }
+}
+
+export default AdicionarProdutoPage;
