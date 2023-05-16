e1ff3380f8c2b5d83e729d9e7b5e38465c5a9c67
José Paulo <jose.silva@bandtec.com.br>
2023-05-15 21:10:51 -0300

    refactor: alterando tamanho da lista de pedidos em preparo

--- campestre-sem-fila/src/components/Pedidos/pedidos.jsx
+++ campestre-sem-fila/src/components/Pedidos/pedidos.jsx
@@ -1,88 +1,89 @@
 import styled from "styled-components";
 
 const Content = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+`;
 
 const PedidosComponent = styled.div`
-    width: 50%;
-    height: 100%;
-    margin-top:
-`
+  width: 50%;
+  height: 100%;
+  margin-top: ;
+`;
 
 const FotoAperetiva = styled.div`
-    width: 50%;
+  width: 50%;
+  height: 100%;
+  img {
     height: 100%;
-    img {
-        height: 100%;
-    }
-`
+  }
+`;
 
 const Header = styled.div`
-    margin-top: 1%;
-    h1 {
-        text-align: center;
-        font-weight: 900;
-    }
-`
+  margin-top: 1%;
+  h1 {
+    text-align: center;
+    font-weight: 900;
+  }
+`;
 
 const NumerosPedidos = styled.div`
-    width: 100%;
-    display: flex;
-`
+  width: 100%;
+  display: flex;
+`;
 
 const PedidosPreparando = styled.div`
-    width: 50%;
-    height: 100%;
-`
+  width: 50%;
+  height: 100%;
+`;
 
 const PedidosProntos = styled.div`
-    width: 50%;
-    height: 100%;
-`
+  width: 50%;
+  height: 100%;
+`;
 
 const TitlePedidoPreparando = styled.h1`
-    text-align: center;
-    background-color: #E4B226;
-    color: white;
-    padding: 2% 0;
-`
+  text-align: center;
+  background-color: #e4b226;
+  color: white;
+  padding: 2% 0;
+`;
 
 const TitlePedidoPronto = styled.h1`
-    text-align: center; 
-    background-color: #DA291C;
-    color: white;
-    padding: 2% 0;  
-`
+  text-align: center;
+  background-color: #da291c;
+  color: white;
+  padding: 2% 0;
+`;
 
 const ListaNumerosPreparo = styled.ul`
+    width: 50%;
     column-count: 2;
     list-style: none;
-`
+`;
 const ListaNumerosPronto = styled.ul`
-    column-count: 2;
-    column-width: 300px;
-    list-style: none;
-    li {
-        margin-top: -1rem;
-    }
-`
+  column-count: 2;
+  column-width: 300px;
+  list-style: none;
+  li {
+    margin-top: -1rem;
+  }
+`;
 
-
-
-export { Content, 
-PedidosComponent, 
-FotoAperetiva, 
-Header, 
-NumerosPedidos, 
-PedidosPreparando, 
-PedidosProntos, 
-TitlePedidoPreparando, 
-TitlePedidoPronto, 
-ListaNumerosPreparo, 
-ListaNumerosPronto }
+export {
+  Content,
+  PedidosComponent,
+  FotoAperetiva,
+  Header,
+  NumerosPedidos,
+  PedidosPreparando,
+  PedidosProntos,
+  TitlePedidoPreparando,
+  TitlePedidoPronto,
+  ListaNumerosPreparo,
+  ListaNumerosPronto,
+};
